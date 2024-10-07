7b66e900d0374447640c3060ff4d7879be69a314
Erik Garcia <erikpumas999@gmail.com>
2024-10-06 18:28:04 -0600

    Primeros pasos

---
+++ app/src/main/java/com/example/reservaciondeviaje/ConfirmationActivity.kt
@@ -0,0 +1,26 @@
+package com.example.reservaciondeviaje
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.reservaciondeviaje.databinding.ActivityConfirmationBinding
+
+class ConfirmationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityConfirmationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityConfirmationBinding.inflate(layoutInflater)
+
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
